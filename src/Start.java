7e6f6f856f972747cc0fd8f05cc02522f22f1110
taigazeal <zeal24460149@fsgcl.jp>
2025-05-26 11:06:03 +0900

    楕円と文字列

--- src/Start.java
+++ src/Start.java
@@ -2,8 +2,13 @@
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("古川大雅");
-		MyFrame frame1 = new RectTest();/// TODO 自動生成されたメソッド・スタブ
+		//System.out.println("古川大雅");
+		//MyFrame frame1 = new RectTest();/
+		//System.out.println("プログラムを実行します");
+		//MyFrame trame1 = new OvalTest();
+		System.out.println("プログラムを実行します");
+		MyFrame trame1 = new TextTest();
+// TODO 自動生成されたメソッド・スタブ
 
 	}
 
