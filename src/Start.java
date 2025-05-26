b64f5496ac519a47c8e3da813e1a9fca90026c48
taigazeal <zeal24460149@fsgcl.jp>
2025-05-26 10:19:13 +0900

    色追加

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,8 @@
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("古川大雅");/// TODO 自動生成されたメソッド・スタブ
+		System.out.println("古川大雅");
+		MyFrame frame1 = new RectTest();/// TODO 自動生成されたメソッド・スタブ
 
 	}
 
